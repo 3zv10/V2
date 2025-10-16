9c1a3cdb807d345e0e2d8f5fe371fd00073cf04a
3zv10 <jcp7dyg@gmail.com>
2025-10-16 16:00:20 +0300

    fix: add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/V2/", // 👈 هذا السطر المهم لموقع GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
